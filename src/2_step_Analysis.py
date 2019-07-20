430f4750be5364d897cdd8ec13f287c214b93a96
ShaotingZhang <saicomb1990@gmail.com>
2019-07-19 19:01:02 -0500

    change code

--- src/2_step_Analysis.py
+++ src/2_step_Analysis.py
@@ -14,7 +14,7 @@ import colorsys
 import numpy.linalg as la
 
 
-def print_top(G,v, num=10):
+def print_top(G, v, num=10):
     idx_list = [(i,v[i]) for i in range(len(v))]
     idx_list = sorted(idx_list, key = lambda x: x[1], reverse=True)
     for i in range(min(num,len(idx_list))):
@@ -24,12 +24,12 @@ def print_top(G,v, num=10):
 def index_of_max(v):
     return numpy.where(v == max(v))[0]
 
-def calc_powerlaw(G,kmin):
+def calc_powerlaw(G, kmin):
     N = G.num_nodes
     ddist = zen.degree.ddist(G,normalize=False)
     cdist = zen.degree.cddist(G,inverse=True)
     k = numpy.arange(len(ddist))
-    
+
     plt.figure(figsize=(10,8))
     plt.subplot(211)
     plt.bar(k,ddist, width=0.8, bottom=0, color='b')
@@ -60,57 +60,11 @@ def calc_powerlaw(G,kmin):
     print ('alpha is %1.2f' %alpha) 
     plt.show()
 
-def modularity(G,c):
-    d = dict()
-    for k,v in c.iteritems():
-        for n in v:
-            d[n] = k
-    Q, Qmax = 0,1
-    for u in G.nodes_iter():
-        for v in G.nodes_iter():
-            if d[u] == d[v]:
-                Q += ( int(G.has_edge(v,u)) - G.in_degree(u)*G.out_degree(v)/float(G.num_edges) )/float(G.num_edges)
-                Qmax -= ( G.in_degree(u)*G.out_degree(v)/float(G.num_edges) )/float(G.num_edges)
-    return Q, Qmax
 
-def propagate(G,d3,x,steps,slp=0.5,keep_highlights=False,update_at_end=False):
-    interactive = d3.interactive
-    d3.set_interactive(False)
-    A = G.matrix().T  # adjacency matrix of the network G
-    d3.highlight_nodes_(list(where(x>0)[0]))
-    d3.update()
-    sleep(slp)
-    cum_highlighted = sign(x)
-    for i in range(steps): # the brains
-        x = sign(dot(A,x)) # the brains
-        cum_highlighted = sign(cum_highlighted+x)
-        if not update_at_end:
-            if not keep_highlights:
-                d3.clear_highlights()
-            d3.highlight_nodes_(list(where(x>0)[0]))
-            d3.update()
-            sleep(slp)
-    if update_at_end:
-        if not keep_highlights:
-            d3.clear_highlights()
-            d3.highlight_nodes_(list(where(x>0)[0]))
-        else:
-            d3.highlight_nodes_(list(where(cum_highlighted>0)[0]))
-        d3.update()
-    d3.set_interactive(interactive)
-    if keep_highlights:
-        return cum_highlighted
-    else:
-        return x
 
 def main():
     G = zen.io.gml.read('GameOfThrones.gml',weight = True, directed = False)
 
-
-    #d3 = d3js.D3jsRenderer(G, interactive=False, autolaunch=False)
-    d3 = d3js.D3jsRenderer(G, interactive=False)
-    d3.update()
-    sleep(1)
 
 
     A = G.matrix()
@@ -151,38 +105,6 @@ def main():
     print ('Clustering: %s' % c)
 
 
-    print ('\n==============================================')
-    print ('\nDiameter and Path')
-    D,P = zen.algorithms.shortest_path.all_pairs_dijkstra_path_(G)
-    d = 0
-    for i in range(len(D)):
-            for j in range(len(D[0])):
-                    if D[i][j] > d:
-                            d = D[i][j]
-
-
-    uidx,vidx = where(D==d)
-    path = zen.algorithms.shortest_path.pred2path_(uidx[1],vidx[1],P)
-#    print path
-#    pathname = []
-#    for i in path:
-#            a = G.node_object(i)
-#            pathname.append(a)
-#    d = len(path)
-#    print ('The network has a diameter of %i.' % d)
-#    print (pathname)
-           
-    # code to visualize the path for i in range(len(path) + 1):
-#    for i in range(len(path) - 1):
-#         a = path[i]
-#         b = path[i + 1]
-#         c = G.edge_idx_(a,b)    
-#         d3.highlight_edges_([c])
-#         d3.update()
-    sleep(3) 
-
-
-    d3.stop_server()
 
 if __name__ == '__main__':
     main()
--- src/3_step_Diffusion.py
+++ src/3_step_Diffusion.py
@@ -17,38 +17,39 @@ def RGBToHTMLColor(rgb_tuple):
 	""" convert an (R, G, B) tuple to #RRGGBB """
 	hexcolor = '#%02x%02x%02x' % rgb_tuple
 	# that's it! '%02x' means zero-padded, 2-digit hex values
-	return hexcolor
+    return hexcolor
 
 def HTMLColorToRGB(colorstring):
 	""" convert #RRGGBB to an (R, G, B) tuple """
-	colorstring = colorstring.strip()
-	if colorstring[0] == '#': colorstring = colorstring[1:]
-	if len(colorstring) != 6:
-		raise ValueError, "input #%s is not in #RRGGBB format" % colorstring
-	r, g, b = colorstring[:2], colorstring[2:4], colorstring[4:]
-	r, g, b = [int(n, 16) for n in (r, g, b)]
-	return (r, g, b)
+    colorstring = colorstring.strip()
+    if colorstring[0] == '#':
+        colorstring = colorstring[1:]
+    if len(colorstring) != 6:
+	raise ValueError, "input #%s is not in #RRGGBB format" % colorstring
+    r, g, b = colorstring[:2], colorstring[2:4], colorstring[4:]
+    r, g, b = [int(n, 16) for n in (r, g, b)]
+    return (r, g, b)
 
 def color_interp(color1,color2,v,m=0,M=1):
-	c1 = array(HTMLColorToRGB(color1))
-	c2 = array(HTMLColorToRGB(color2))
-	if v > M:
-		c = tuple(c2)
-	elif v < m:
-		c = tuple(c1)
-	else:
-		#c = tuple( c1 + (c2-c1)/(M-m)*(v-m) ) # linear interpolation of color
-		c = tuple( c1 + (c2-c1)*(1 - exp(-2*(v-m)/(M-m))) ) # logistic interpolation of color
-	return RGBToHTMLColor(c)
+    c1 = array(HTMLColorToRGB(color1))
+    c2 = array(HTMLColorToRGB(color2))
+    if v > M:
+        c = tuple(c2)
+    elif v < m:
+        c = tuple(c1)
+    else:
+	    #c = tuple( c1 + (c2-c1)/(M-m)*(v-m) ) # linear interpolation of color
+	c = tuple( c1 + (c2-c1)*(1 - exp(-2*(v-m)/(M-m))) ) # logistic interpolation of color
+    return RGBToHTMLColor(c)
 
 def color_by_value(d3,G,x,color1='#77BEF5',color2='#F57878'):
-	d3.set_interactive(False)
-	m = min(x)
-	M = max(x)
-	for i in G.nodes_iter_():
-		d3.stylize_node_(i, d3js.node_style(fill=color_interp(color1,color2,x[i])))
-	d3.update()
-	d3.set_interactive(True)
+    d3.set_interactive(False)
+    m = min(x)
+    M = max(x)
+    for i in G.nodes_iter_():
+        d3.stylize_node_(i, d3js.node_style(fill=color_interp(color1,color2,x[i])))
+    d3.update()
+    d3.set_interactive(True)
 
 
 
@@ -89,9 +90,9 @@ def main():
 
 
     for i,t in enumerate(time):      # each time
-            x = x + 0.1 * numpy.dot((A - D),x) * dt    # compute  state X
-            color_by_value(d3,G,x)
-            sleep(0.8)
+        x = x + 0.1 * numpy.dot((A - D),x) * dt    # compute  state X
+        color_by_value(d3,G,x)
+        sleep(0.8)
             
     d3.stop_server()
 
